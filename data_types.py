8f1e7155abcb51f14594baf521445c0ab38f87df
Ronald Laukaitis <rol226@lehigh.edu>
2024-08-02 13:25:56 -0700

    Portals and different heights

--- data_types.py
+++ data_types.py
@@ -1,10 +1,18 @@
 from settings import *
 
 class Segment:
-    def __init__(self,p0: tuple[float], p1: tuple[float]):
+    def __init__(self,p0: tuple[float], p1: tuple[float], sector_id = None, back_sector_id = None, low_tex_id = None, mid_tex_id = None, up_tex_id = None):
         
         self.pos: tuple[vec2] = vec2(p0), vec2(p1)
         self.vector: vec2 = self.pos[1] - self.pos[0]
+        
+        self.sector_id: int = sector_id
+        self.back_sector_id: int = back_sector_id
+        
+        self.low_tex_id: int = low_tex_id
+        self.mid_tex_id: int = mid_tex_id
+        self.up_tex_id: int = up_tex_id
+        
         
         self.wall_model_id: set[int] = set()
         
@@ -19,3 +27,14 @@ class BSPNode:
         self.splitter_vec: vec2 = None
         
         self.segment_id: int = None
+
+class Sector:
+    def __init__(self, floor_h=None, ceil_h=None, floor_tex_id = None, ceil_tex_id=None):
+        
+        self.floor_h: float = floor_h
+        self.ceil_h: float = ceil_h
+        
+        self.floor_tex_id: int = floor_tex_id
+        self.ceil_tex_id: int = ceil_tex_id
+        
+        
--- level_data.py
+++ level_data.py
@@ -8,3 +8,31 @@ class LevelData:
         
         self.raw_segments = [Segment(p0,p1) for (p0,p1) in SEGMENTS]
         self.settings = SETTINGS
+        
+        self.sector_data = SECTOR_DATA
+        self.segments_of_sector_boundaries = SEGMENTS_OF_SECTOR_BOUNDARIES
+        
+        self.sectors = {}
+        self.handle_sector_data()
+        
+        self.raw_segments = []
+        self.handle_segments_of_sector_boundaires()
+        
+    def handle_sector_data(self):
+        for sec_id, sector_data in self.sector_data.items():
+            
+            sector = Sector(
+                floor_h= sector_data['floor_h'],
+                ceil_h= sector_data['ceil_h'],
+                floor_tex_id=sector_data.get('floor_tex_id', 0)
+                ceil_tex_id=sector_data.get('ceil_tex_id', 0),
+            )
+            self.sectors[sec_id] = sector
+    
+    def handle_segments_of_sector_boundaries(self):
+        for (p0,p1), sector_ids, textures in self.segments_of_sector_boundaries:
+            
+            seg = self.get_segment(p0, p1, sector_ids, textures)
+            self.raw_segments.append(seg)
+            
+    def get_segment
